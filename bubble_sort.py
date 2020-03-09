1999e9f84699a27f47519c22205590f65f93f325
kangah-codes <Joshua Akangah>
2020-03-09 23:58:49 +0000

    Bubble sort

---
+++ bubble_sort.py
@@ -0,0 +1,16 @@
+def bubble_sort(iterable):
+   length = len(iterable)
+
+   for i in range(0, length-1):
+      swapped = f=False
+
+      for j in range(0, length-1):
+         if iterable[j] > iterable[j+1]:
+            iterable[j], iterable[j+1] = iterable[j+1], iterable[j]
+            swapped = True
+      if not swapped:
+         break
+
+   return iterable
+
+print(bubble_sort([345,634,345,5,234534,634]))
