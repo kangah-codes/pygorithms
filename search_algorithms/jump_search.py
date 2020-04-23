e7733c74a3a9bbc8d56db10d7a0c5752e12623a5
Joshua Akangah <akangah89@hmail.com>
2020-04-23 05:25:25 +0000

    Added jump search

---
+++ search_algorithms/jump_search.py
@@ -0,0 +1,24 @@
+import math
+def jump_search(iterable, search):
+	n = len(iterable)
+	jump = math.sqrt(n)
+	prev = 0
+
+	while iterable[int(min(jump, n)-1)] < search:
+		prev = jump
+		jump += math.sqrt(n)
+		if prev >= n:
+			return -1
+
+	while iterable[int(prev)] < search:
+		prev += 1
+		if prev == min(jump, n):
+			return -1
+
+	if iterable[int(prev)] == search:
+		return int(prev)
+
+	return -1
+
+print(jump_search([3,4,5,6,6,77,84,455], 77))
+
