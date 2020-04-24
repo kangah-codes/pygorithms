d17e71439465c4a1971abada0c6945f21164c9b8
Joshua Akangah <akangah89@hmail.com>
2020-04-24 12:30:00 +0000

    Added quicksort

---
+++ sorting_algorithms/quicksort.py
@@ -0,0 +1,19 @@
+def quicksort(iterable, l, h):
+	if l < h:
+		p = partition(iterable, l, h)
+		quicksort(iterable, l, p-1)
+		quicksort(iterable, p+1, h)
+
+def partition(iterable, l, h):
+	pivot = iterable[h]
+	i = l
+	for j in range(l, h+1):
+		if iterable[j] < pivot:
+			iterable[j], iterable[i] = iterable[i], iterable[j]
+			i += 1
+	iterable[i], iterable[h] = iterable[h], iterable[i]
+	return i
+
+a = [1,2,43543,234,54,0,-1,5]
+quicksort(a, 0,len(a)-1)
+print(a)
