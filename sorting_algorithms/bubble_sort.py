e461d71c00d840dd3d312e0d895af1ae7e067557
Joshua Akangah <akangah89@hmail.com>
2020-04-27 23:43:36 +0000

    Fixed merge sort

--- sorting_algorithms/bubble_sort.py
+++ sorting_algorithms/bubble_sort.py
@@ -4,7 +4,7 @@ def bubble_sort(iterable):
    length = len(iterable)
 
    for i in range(0, length-1):
-      swapped = f=False
+      swapped = f = False
 
       for j in range(0, length-1):
          if iterable[j] > iterable[j+1]:
--- sorting_algorithms/merge_sort.py
+++ sorting_algorithms/merge_sort.py
@@ -1,31 +1,51 @@
-"""
-MergeSort(arr[], l,  r)
-If r > l
-     1. Find the middle point to divide the array into two halves:  
-             middle m = (l+r)/2
-     2. Call mergeSort for first half:   
-             Call mergeSort(arr, l, m)
-     3. Call mergeSort for second half:
-             Call mergeSort(arr, m+1, r)
-     4. Merge the two halves sorted in step 2 and 3:
-             Call merge(arr, l, m, r)
-"""
-
-left_stack = []
-
-def merge_sort(iterable):
-	length = len(iterable)
-	if length % 2 == 0:
-		middle = length // 2
-	else:
-		middle = (length+1) // 2
-	left = iterable[:middle]
-	right = iterable[middle:]
-
-	merge_sort(left)
-	merge_sort(right)
-
-	print(left, right)
-
-merge_sort([4,23,5676,23,43])
-
+def merge(arr, l, m, r): 
+    n1 = m - l + 1
+    n2 = r- m 
+  
+    # create temp arrays 
+    L = [0] * (n1) 
+    R = [0] * (n2) 
+  
+    for i in range(0 , n1): 
+        L[i] = arr[l + i] 
+  
+    for j in range(0 , n2): 
+        R[j] = arr[m + 1 + j] 
+  
+    i = 0     # Initial index of first subarray 
+    j = 0     # Initial index of second subarray 
+    k = l     # Initial index of merged subarray 
+  
+    while i < n1 and j < n2 : 
+        if L[i] <= R[j]: 
+            arr[k] = L[i] 
+            i += 1
+        else: 
+            arr[k] = R[j] 
+            j += 1
+        k += 1
+  
+    while i < n1: 
+        arr[k] = L[i] 
+        i += 1
+        k += 1
+  
+    while j < n2: 
+        arr[k] = R[j] 
+        j += 1
+        k += 1
+  
+def mergeSort(arr,l,r): 
+    if l < r:
+        m = (l+(r-1))//2
+  
+        mergeSort(arr, l, m) 
+        mergeSort(arr, m+1, r) 
+        merge(arr, l, m, r) 
+  
+  
+arr = [12, 11, 13, 5, 6, 7] 
+n = len(arr) 
+  
+mergeSort(arr,0,n-1) 
+print(arr)
